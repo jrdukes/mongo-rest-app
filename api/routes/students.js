f0c77d71758f7e04baf4112dad750420157a986d
Joe Dukes <joedukes.email@gmail.com>
2019-06-29 10:12:11 -0400

    Updates to students.js, server.js, index.html, main.js, styles.css

--- api/routes/students.js
+++ api/routes/students.js
@@ -1,49 +1,52 @@
-const express = require('express')
-const Student = require('../models/Student')
-const router = express.Router()
+const express = require('express');
+const Student = require('../models/Student');
+const router = express.Router();
+
+// Refactoring for async/await from https://github.com/AnnaCate/jrs-mongo-rest
 
 // /students/
 router
-	.route('/')
-	.get(function(request, response) {
-		Student.find({}).then(function(students) {
-			response.json({ status: 'ok', data: students })
-		})
-	})
-	.post(function(request, response) {
-		const rawStudent = request.body
+  .route('/')
+  .get(function(request, response) {
+    Student.find({}).then(function(students) {
+      response.json({ status: 'ok', data: students });
+    });
+  })
+  .post(function(request, response) {
+    const rawStudent = request.body;
 
-		const newStudent = new Student(rawStudent)
+    const newStudent = new Student(rawStudent);
 
-		newStudent.save()
+    newStudent.save();
 
-		response.json({ status: 'ok', newStudent })
-	})
+    response.json({ status: 'ok', newStudent });
+  });
 
 // /students/:studentId
+
 router
-	.route('/:studentId')
-	.get(function(request, response) {
-		Student.findById(request.params.studentId).then(function(foundStudent) {
-			response.json(foundStudent)
-		})
-	})
-	.put(function(request, response) {
-		Student.findById(request.params.studentId).then(function(foundStudent) {
-			foundStudent.name = request.body.name
-			foundStudent.age = request.body.age
-			foundStudent.photoUrl = request.body.photoUrl
-			foundStudent.bio = request.body.bio
+  .route('/:studentId')
+  .get(function(request, response) {
+    Student.findById(request.params.studentId).then(function(foundStudent) {
+      response.json(foundStudent);
+    });
+  })
+  .put(async (req, res) => {
+    await Student.findById(req.params.studentId).then(foundStudent => {
+      foundStudent.name = req.body.name;
+      foundStudent.age = req.body.age;
+      foundStudent.photoUrl = req.body.photoUrl;
+      foundStudent.bio = req.body.bio;
 
-			foundStudent.save()
+      foundStudent.save();
 
-			response.json(foundStudent)
-		})
-	})
-	.delete(function(request, response) {
-		Student.findByIdAndDelete(request.params.studentId).then(function(res) {
-			response.json({ status: 'ok', res: res })
-		})
-	})
+      res.json(foundStudent);
+    });
+  })
+  .delete(async (req, response) => {
+    await Student.findByIdAndDelete(req.params.studentId).then(res => {
+      response.json({ status: 'ok', res: req.params.studentId });
+    });
+  });
 
-module.exports = router
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,19 +1,28 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const StudentRoutes = require('./routes/students')
-mongoose.connect('mongodb://localhost:27017/students', { useNewUrlParser: true })
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const StudentRoutes = require('./routes/students');
+mongoose.connect('mongodb://localhost:27017/students', {
+  useNewUrlParser: true
+});
 
-const app = express()
-app.use(cors())
-app.use(bodyParser())
-app.get('/', function(request, response) {
-	console.log('We live in the GET route')
-	response.json({ status: 'ok', message: 'Welcome to my server.' })
-})
-app.use('/students', StudentRoutes)
+// from https://github.com/AnnaCate/jrs-mongo-rest
+// fix deprecation warning:
+
+mongoose.set('useCreateIndex', true);
+
+const app = express();
+app.use(cors());
+// app.use(bodyParser()); DEPRECATED - use the 2 lines below instead
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) =>
+  res.json({ status: 'ok', message: 'Welcome to my server.' })
+);
+app.use('/students', StudentRoutes);
 
 app.listen(8000, function() {
-	console.log('Server is listening on PORT 8000 -- hello everyone')
-})
+  console.log('Server is listening on PORT 8000 -- hello everyone');
+});
